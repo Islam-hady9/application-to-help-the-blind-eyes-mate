6a50d2c5ff18a48ef1515743dabe87dce98a8871
Islam Abd_Elhady <eslamabdo71239@gmail.com>
2024-06-30 02:54:14 +0300

    Add swipe-based navigation system

--- eyes_mate_app/lib/main.dart
+++ eyes_mate_app/lib/main.dart
@@ -1,125 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  FlutterTts flutterTts = FlutterTts();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _speakHello();
+  }
+
+  Future<void> _speakHello() async {
+    await flutterTts.speak(
+        "Hello... Swipe Up To Open Object Recognition and Distance Measurement, Swipe Down To Open Text Reading, Swipe Left To Open Currency Recognition, Swipe Right To Open Face Recognition");
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home Page'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SwipePage(),
+    );
+  }
+}
+
+class SwipePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragEnd: (details) {
+        // If the user swipes up, navigate to UpPage
+        if (details.velocity.pixelsPerSecond.dy < 0) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => UpPage(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          );
+        }
+        // If the user swipes down, navigate to DownPage
+        else if (details.velocity.pixelsPerSecond.dy > 0) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DownPage(),
             ),
-          ],
+          );
+        }
+      },
+      onHorizontalDragEnd: (details) {
+        // If the user swipes to the right, navigate to RightPage
+        if (details.velocity.pixelsPerSecond.dx > 0) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => RightPage(),
+            ),
+          );
+        }
+        // If the user swipes to the left, navigate to LeftPage
+        else if (details.velocity.pixelsPerSecond.dx < 0) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => LeftPage(),
+            ),
+          );
+        }
+      },
+      child: Container(
+        color: Color.fromARGB(255, 182, 54, 67),
+        child: Center(
+          child: Text(
+            'Swipe up, down, left, or right',
+            style:
+                TextStyle(color: Color.fromARGB(255, 6, 1, 1), fontSize: 20.0),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DownPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Down Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Down Page',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
+
+class UpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Up Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Up Page',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
+
+class LeftPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Left Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Left Page',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
+
+class RightPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Right Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Right Page',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
     );
   }
 }
